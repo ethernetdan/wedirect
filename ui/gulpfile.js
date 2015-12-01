7c7299d22dfc9ab654a8658cd3452c9bcb53ef7b
jaxgeller <jacksongeller@gmail.com>
2015-12-01 01:51:30 -0500

    downgrade gulpfile from babel to es5, reduce amount of dependencies, version dependencies

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,26 +1,11 @@
-// IMPORTS
+'use strict';
 
-import babelify from 'babelify'
-import browserify from 'browserify'
-import gulp from 'gulp'
-import autoprefixer from 'gulp-autoprefixer'
-import changed from 'gulp-changed'
-import connect from 'gulp-connect'
-import fileinclude from 'gulp-file-include'
-import htmlmin from 'gulp-htmlmin'
-import imagemin from 'gulp-imagemin'
-import minify from 'gulp-minify-css'
-import plumber from 'gulp-plumber'
-import sass from 'gulp-sass'
-import sourcemaps from 'gulp-sourcemaps'
-import uglify from 'gulp-uglify'
-import assign from 'lodash.assign'
-import notifier from 'node-notifier'
-import buffer from 'vinyl-buffer'
-import source from 'vinyl-source-stream'
-import watchify from 'watchify'
-
-// ERROR HANDLER
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer')
+const connect = require('gulp-connect')
+const plumber = require('gulp-plumber')
+const sass = require('gulp-sass')
+const notifier = require('node-notifier')
 
 const onError = function(error) {
   notifier.notify({
@@ -32,17 +17,12 @@ const onError = function(error) {
   this.emit('end')
 }
 
-// HTML
-
 gulp.task('html', () => {
   return gulp.src('src/html/**/*.html')
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(fileinclude({ prefix: '@' }))
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload())
 })
-
-// JS
 
 gulp.task('js', () => {
   return gulp.src('src/js/**/*.js')
@@ -51,27 +31,20 @@ gulp.task('js', () => {
     .pipe(connect.reload())
 })
 
-// SASS
-
 gulp.task('sass', () => {
   return gulp.src('src/sass/style.scss')
     .pipe(plumber({ errorHandler: onError }))
     .pipe(sass())
     .pipe(autoprefixer({ browsers: [ 'last 2 versions', 'ie >= 9', 'Android >= 4.1' ] }))
-    .pipe(minify())
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload())
 })
-
-// WATCH
 
 gulp.task('watch', () => {
   gulp.watch('src/html/**/*.html', ['html'])
   gulp.watch('src/sass/**/*.scss', ['sass'])
   gulp.watch('src/js/**/*.js', ['js'])
 })
-
-// TASKS
 
 gulp.task('build', ['html', 'sass', 'js'])
 gulp.task('default', ['build', 'watch'])
